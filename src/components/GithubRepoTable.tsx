5f99074f0250f118c1a0bbf4904d6101d3e1df5d
Richard Tan <hjtan3333@gmail.com>
2023-01-18 17:00:06 +0800

    feat: table pagination feature

--- src/components/GithubRepoTable.tsx
+++ src/components/GithubRepoTable.tsx
@@ -3,26 +3,40 @@ import styled from "styled-components";
 import {
   useTable,
   useGlobalFilter,
+  usePagination,
   useAsyncDebounce,
   Row,
   TableInstance,
+  UseGlobalFiltersInstanceProps,
+  UsePaginationInstanceProps,
 } from "react-table";
 
 import { GithubRepoTable } from "../interfaces/GithubRepo";
 
 const StyledTable = styled.div``;
 
-interface useTableProps extends TableInstance {
-  preGlobalFilteredRows?: Array<Row>;
-  setGlobalFilter?: () => void;
+// Inteface merging for custom option as per say in react-table types
+interface useTableProps
+  extends TableInstance,
+    Partial<UsePaginationInstanceProps<{}>>,
+    Partial<UseGlobalFiltersInstanceProps<{}>> {
   state: any;
+  flatRows: TableInstance["flatRows"];
+  rows: TableInstance["rows"];
+  rowsById: TableInstance["rowsById"];
+}
+
+interface searchFilter {
+  preGlobalFilteredRows: Row[];
+  globalFilter: any;
+  setGlobalFilter: (filterValue: any) => void;
 }
 
 const SearchFilter = ({
   preGlobalFilteredRows,
   globalFilter,
   setGlobalFilter,
-}: any) => {
+}: searchFilter) => {
   const count = preGlobalFilteredRows.length;
   const [value, setValue] = useState(globalFilter);
   const onChange = useAsyncDebounce((value) => {
@@ -49,37 +63,60 @@ const Table: FC<GithubRepoTable> = ({ columns, data }) => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
     visibleColumns,
-    state,
+    state: { pageIndex, pageSize, globalFilter },
+    // search filters
     preGlobalFilteredRows,
     setGlobalFilter,
+    // pagination filters
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
   }: useTableProps = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 0 } as any,
     },
-    useGlobalFilter
+    useGlobalFilter,
+    usePagination
   );
+
+  const isSearchReady = preGlobalFilteredRows && setGlobalFilter !== undefined;
+  const isPaginationReady =
+    gotoPage &&
+    previousPage &&
+    nextPage &&
+    pageOptions &&
+    pageCount &&
+    setPageSize;
 
   return (
     <StyledTable>
       <table {...getTableProps()}>
         <thead>
           <tr>
-            <th
-              colSpan={visibleColumns.length}
-              style={{
-                textAlign: "left",
-              }}
-            >
-              <SearchFilter
-                preGlobalFilteredRows={preGlobalFilteredRows}
-                globalFilter={state.globalFilter}
-                setGlobalFilter={setGlobalFilter}
-              />
-            </th>
+            {isSearchReady ? (
+              <th
+                colSpan={visibleColumns.length}
+                style={{
+                  textAlign: "left",
+                }}
+              >
+                <SearchFilter
+                  preGlobalFilteredRows={preGlobalFilteredRows}
+                  globalFilter={globalFilter}
+                  setGlobalFilter={setGlobalFilter}
+                />
+              </th>
+            ) : null}
           </tr>
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
@@ -90,20 +127,70 @@ const Table: FC<GithubRepoTable> = ({ columns, data }) => {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
+          {page &&
+            page.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
         </tbody>
       </table>
+      {isPaginationReady ? (
+        <div className="pagination">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {"<<"}
+          </button>{" "}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {"<"}
+          </button>{" "}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {">"}
+          </button>{" "}
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>{" "}
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <span>
+            | Go to page:{" "}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px" }}
+            />
+          </span>{" "}
+          <select
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      ) : null}
     </StyledTable>
   );
 };
